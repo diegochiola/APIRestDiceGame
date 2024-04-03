aadb5a64b5f639e83ea4e15c8e958b9a1b771971
diega <diegochiola@gmail.com>
2024-04-04 00:27:34 +0200

    Create files: Seeders and Policies

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Game;
 use Illuminate\Database\Seeder;
+use Database\Seeders\UserSeeder;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,10 +17,13 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
-
+        $this->call(RoleSeeder::class);
+        $this->call(GameSeeder::class);
+        $this->call(UserSeeder::class);
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        
     }
 }
---
+++ database/seeders/GameSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Game;
+
+class GameSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //Si llamo al factory es menos codigo
+        Game::factory(10)->create();
+        /*Game::create([
+            'dice1' => 2, 'dice2' => 6,
+            'won' => false,
+            'user_id' => 1, 
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Game::create([
+            'dice1' => 2, 'dice2' => 5,
+            'won' => true,
+            'user_id' => 1, 
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Game::create([
+            'dice1' => 3, 'dice2' => 4,
+            'won' => true,
+            'user_id' => 2, 
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Game::create([
+            'dice1' => 4, 'dice2' => 6,
+            'won' => false,
+            'user_id' => 2, 
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        */
+    }
+}
