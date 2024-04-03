4021a46860e272e88a3fcce5931ed361cda445f0
diega <diegochiola@gmail.com>
2024-04-03 23:19:20 +0200

    Controllers done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,35 +3,103 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Workbench\App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
     //definir funciones
 
     public function registerUser(Request $request)
-    {
-        return $request;
+    { 
+        $validator = Validator::make($request->all(), [
+            'name' => 'unique:users',
+            'nickname' => 'nullable|unique:users',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|alpha_num'
+        ]);
+    
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+        //si son validos
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']); //cifrar la password con bcrypt
+        $user = User::create($input);
+        //asignar el rol
+        $this->assignPlayerRoleToUser($user);
+         //asignar token 
+        $success['token'] = $user->createToken('authToken')->accessToken;
+        $success['id'] = $user->id;
+        $success['name'] = $user->name;
+        $success['role'] = 'player';
+    
+        return response()->json($user, 201);
     }
 
-
-    
     public function createUser(Request $request)
     {
-        return $request;
+        $user = User::create([
+            'name' => $request->name,
+            'nickname' => $nickname,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        return $user;
     }
 
-    public function asignRoleToUser(Request $request)
+    public function assignRoleToUser($user)
     {
-        return $request;
+        $role = Role::findByName('player');
+        $user->assignRole($role);
+
     }
-    public function loginUser(Request $request)
+    public function update(UpdateUserRequest $request, User $user){
+        $user->update($request->all());
+       }
+    
+       public function updateName(Request $request, $id)
     {
-        return $request;
+        $user = Auth::user();
+        if ($user->id != $id) {
+            return response()->json(['error' => 'You are not authorized for this action'], 403);
+        }
+        $this->validate($request, [
+            'name' => 'required',
+            'nickname' => 'required'
+        ]);
+        $user->name = $request->input('name');
+        $user->nickname = $request->input('nickname');
+        $user->save();
+        return response()->json(['message' => 'Name updated successfully!', 'user' => $user], 200);
     }
-    public function logOutUser(Request $request)
-    {
-        return $request;
-    }
+
+    public function login(Request $request) {
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)){
+            $user = Auth::user();
+            $success['token'] = $user->createToken('authToken')->accessToken;
+            $success['name'] = $user->name;
+            return response()->json([$success, 'You are logged in!'], 200);
+        } else {
+            return response()->json(['error' => 'Login error. Email or password error.'], 401);
+        }
+     }
+     public function logout()
+     {
+         $user = Auth::user();
+    
+         if ($user) {
+    
+             $user->tokens->each->revoke();
+    
+             return response()->json('Log out successfully!', 200);
+         } else {
+             return response()->json('You are not logged in.', 401);
+         }
+     }
 
     //calculos para ranking en game controller
 
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,6 +42,7 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Laravel\Passport\Http\Middleware\CreateFreshApiToken::class,
         ],
     ];
 
@@ -64,18 +65,16 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-    ];
-    protected $routeMiddleware = [
+        'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+        'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
         
-        'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
-        'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
-        'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
     ];
-    /*    protected $routeMiddleware = [
+
+    protected $routeMiddleware = [
         // ...
         'checkRole' => \App\Http\Middleware\CheckRole::class,
         'admin' => \App\Http\Middleware\AdminMiddleware::class,
         'player' => \App\Http\Middleware\PlayerMiddleware::class,
     ];
-    */
+    
 }
